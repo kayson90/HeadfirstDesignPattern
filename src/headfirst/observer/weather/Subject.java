93658e9baa9eef6282bbe2b112e0da4bde8c29f3
lvwenkai <lvwenkai@corp.netease.com>
2020-06-23 16:43:13 +0800

    观察者模式继续理解

--- src/headfirst/observer/weather/Subject.java
+++ src/headfirst/observer/weather/Subject.java
@@ -2,6 +2,7 @@ package headfirst.observer.weather;
 
 /**
  * 主题用来，注册，删除，通知
+ * 其实我认为这里的subject没有必要抽象出来
  */
 public interface Subject {
 	public void registerObserver(Observer o);
--- src/headfirst/observer/weather/WeatherStation.java
+++ src/headfirst/observer/weather/WeatherStation.java
@@ -1,9 +1,14 @@
 package headfirst.observer.weather;
 
-import java.util.*;
-
 public class WeatherStation {
-
+	/**
+	 * 观察者模式也很简单，比如我让你做一个订阅、发布的功能，你怎么做？
+	 * 是不是会把观察者收消息定义为接口，
+	 * 是不是会有被观察的，用来维护观察者列表，发布消息
+	 * 所以问题就变得很清晰明了了这里Subject不是一个消息，而是一个被观察者。
+	 * 其实被观察者是不用抽象的，因为只有一个嘛，扩展的可能行也不大。
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		WeatherData weatherData = new WeatherData();
 		//在构造函数中进行的注册
